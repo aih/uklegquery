6881fd4cc13c73a338538c9081841bdfe0260b83
aih <aih@tabulaw.com>
2014-11-22 05:57:52 -0800

    Examples now work

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-    VERSION = '0.0.2'
+    VERSION = '0.0.3'
 
 class ProductionConfig(Config):
     DEBUG = False
--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,7 @@ ukleg.config(['$locationProvider', '$httpProvider', function ($locationProvider,
     $httpProvider.defaults.headers.common = {Accept: 'text/xml, */*'};
 }]);
 
+
 ukleg.controller('rootController', ['$scope', 'queryFactory', 'readerFactory', function ($scope, queryFactory, readerFactory) {
 
    $scope.inputXPath = '';
@@ -18,41 +19,93 @@ ukleg.controller('rootController', ['$scope', 'queryFactory', 'readerFactory', f
    };
    $scope.sendXPathQuery = function(querytext){
        initResults();
+       alertify.log('Looking up results...');
        queryFactory.queryUKLeg(querytext, $scope.options).then(function(ajaxdata){
         $scope.queryResults.xQueryResults = ajaxdata.toString();
-       });
+       }).then(function(){jQuery('.alertify-log').click();});
    };
    var initResults = function(){
         $scope.queryResults = {};
    };
+
+   $scope.setQuery = function(path){
+        $scope.inputXPath = path;
+   }
+
+   /**
+    * Array of examples
+    */
+   $scope.examples = queryFactory.examples;
 }]);
 
 ukleg.factory('queryFactory', function ($http, $q){
+    var namespaces ={
+        akn:'"http://docs.oasis-open.org/legaldocml/ns/akn/3.0/CSD11"',
+        response:'"http://exist-db.org/xquery/response"',
+        dct:'"http://purl.org/dc/terms/"',
+        ukm:'"http://www.legislation.gov.uk/namespaces/metadata"',
+        ukl:'"http://www.legislation.gov.uk/namespaces/legislation"',
+        atom:'"http://www.w3.org/2005/Atom"',
+        dc:'"http://purl.org/dc/elements/1.1/"'
+    };
+    var namespaceDecs = '';
+    for(var key in namespaces){
+        namespaceDecs=namespaceDecs+'declare namespace '+key+'='+namespaces[key]+';';
+    }
     var basedomain= 'http://54.172.251.13:8080';
     var basequeryurl= '/exist/rest/db/ukpga';
 	var modules= 'import module namespace response = "http://exist-db.org/xquery/response";';
-    var namespaceDec='declare namespace akn="http://docs.oasis-open.org/legaldocml/ns/akn/3.0/CSD11";declare namespace response="http://exist-db.org/xquery/response";';
-	var flwrStart='for $entry in ';
+    var flwrStart='for $entry in ',
+        flwrStartCount='for $entry in count(',
+        flwrEndCount=')';
     var collection = 'collection(\'ukpga\')';
 	var headers = ' let $headers :=(response:set-header("Access-Control-Allow-Origin", "*"))';
-    var returnPhrase = ' return <div class="entry">{$entry}</div>';
+    var returnPhrase = ' return <div class="entry">{$entry}</div>',
+        returnPhraseCount = 'return $count';
+
+
+    var examples = [
+        {"xpath": "//akn:section[ancestor::akn:act//dc:subject='Councils']",
+    "explanation":"Sections with the subject 'Councils'"},
+    {"xpath":"//akn:p[./akn:ref]","explanation":"Paragraphs containing references"},
+    {"xpath":"//akn:section[ancestor::akn:act//dc:subject='Councils']","explanation":"Sections in Acts relating to 'Councils'"},
+    {"xpath":"//akn:section[contains(.,'asbestos')]","explanation":"Sections relating to 'Asbestos'"}
+   ];
+
     //Return public API
         return({
             queryUKLeg: queryUKLeg,
             getCombinedQuery : function(inputXPath){
                 return "collection('ukpga')"+inputXPath;
-            }
+            },
+            examples: examples,
+            setQuery: setQuery
             });
 
+
+        function setQuery(queryPath){
+            return queryPath;
+        };
+
         function queryUKLeg(inputXPath, options){
-            var request = $http({
+            if(options.countResults){
+                var request = $http({
                         method: "get",
                         url: basedomain + basequeryurl,
                         params: {
-                            _query: namespaceDec+flwrStart+collection+inputXPath+headers+returnPhrase,
+                            _query: namespaceDecs+flwrStartCount+collection+inputXPath+flwrEndCount+headers+returnPhrase
+                        }
+                    });
+            }else{
+                var request = $http({
+                        method: "get",
+                        url: basedomain + basequeryurl,
+                        params: {
+                            _query: namespaceDecs+flwrStart+collection+inputXPath+headers+returnPhrase,
                             _howmany: options.howManyResults
                         }
                     });
+            }
 
             return( request.then ( handleSuccess, handleError ) );
         }
