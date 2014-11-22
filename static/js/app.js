7ca818458461ffa6296f0b1491cbfe12960ba8ed
aih <aih@tabulaw.com>
2014-11-22 00:01:35 -0800

    HTTP now working

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@
 ukleg.config(['$locationProvider', '$httpProvider', function ($locationProvider, $httpProvider) {
     
     $locationProvider.html5Mode(true);
+    $httpProvider.defaults.headers.common = {Accept: 'text/xml, */*'};
 }]);
 
 ukleg.controller('rootController', ['$scope', 'queryFactory', function ($scope, queryFactory) {
@@ -13,16 +14,65 @@ ukleg.controller('rootController', ['$scope', 'queryFactory', function ($scope, 
    $scope.getCombinedQuery= function(){
        $scope.combinedQuery = queryFactory.getCombinedQuery($scope.inputXPath);
    };
+   $scope.sendXPathQuery = function(){
+       return queryFactory.queryUKLeg($scope.inputXPath);
+   };
 }]);
 
-ukleg.factory('queryFactory', function (){
+ukleg.factory('queryFactory', function ($http, $q){
+    var basedomain= 'http://54.172.251.13:8080';
+    var basequeryurl= '/exist/rest/db/ukpga';
+    var namespaceDec='declare namespace akn="http://docs.oasis-open.org/legaldocml/ns/akn/3.0/CSD11";for $section in ';
+    var collection = 'collection(\'ukpga\')';
+    var returnPhrase = ' return <entry>{$section}</entry>';
+    //Return public API
+        return({
+            queryUKLeg: queryUKLeg,
+            getCombinedQuery : function(inputXPath){
+                return "collection('ukpga')"+inputXPath;
+            }
+            });
+        /*
+         * PUBLIC METHODS
+         */
 
-    return{
-        getCombinedQuery : function(inputXPath){
-            return "collection('ukpga')"+inputXPath;
+        function queryUKLeg(inputXPath){
+            var request = $http({
+                        method: "get",
+                        url: basedomain + basequeryurl,
+                        params: {
+                            _query: namespaceDec+collection+inputXPath+returnPhrase
+                        }
+                    });
+
+            return( request.then ( handleSuccess, handleError ) );
         }
-    };
-});
+
+        /*
+         * PRIVATE METHODS
+         */
+        function handleError( response ) {
+            jQuery('.alertify-log').click();
+            alertify.error('(status: '+response.status+') An error occurred: '+ response.statusText);
+            if (
+                    ! angular.isObject( response.data ) ||
+                    ! response.data.message
+                    ) {
+                        return( $q.reject( "An unknown error occurred") );
+                    }
+ 
+                    // Otherwise, use expected error message.
+                    return( $q.reject( response.data.message ) );
+ 
+                }
+
+        function handleSuccess( response ) {
+ 
+                    return( response.data );
+ 
+                }
+
+        });
 
 
 })();
