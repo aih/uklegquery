bd6521085ecc5ad759cd4c29d18763de565f00b3
aih <aih@tabulaw.com>
2014-11-22 04:08:46 -0800

    Added options for queries. CSS improvements. Bump to v0.0.2

--- static/js/app.js
+++ static/js/app.js
@@ -10,18 +10,20 @@ ukleg.config(['$locationProvider', '$httpProvider', function ($locationProvider,
 ukleg.controller('rootController', ['$scope', 'queryFactory', 'readerFactory', function ($scope, queryFactory, readerFactory) {
 
    $scope.inputXPath = '';
-   $scope.ukleg={};
+   $scope.queryResults={};
+   $scope.options={howManyResults: 100};
    $scope.combinedQuery = '';
    $scope.getCombinedQuery= function(){
        $scope.combinedQuery = queryFactory.getCombinedQuery($scope.inputXPath);
    };
    $scope.sendXPathQuery = function(querytext){
-       $scope.test = 'mytest';
-       $scope.ukleg.queryURL = queryFactory.getQueryURL(querytext);
-       queryFactory.queryUKLeg(querytext).then(function(ajaxdata){
-        $scope.ukleg.xQueryResults = ajaxdata.toString();
-        console.log($scope.ukleg.xQueryResults);
+       initResults();
+       queryFactory.queryUKLeg(querytext, $scope.options).then(function(ajaxdata){
+        $scope.queryResults.xQueryResults = ajaxdata.toString();
        });
+   };
+   var initResults = function(){
+        $scope.queryResults = {};
    };
 }]);
 
@@ -30,31 +32,25 @@ ukleg.factory('queryFactory', function ($http, $q){
     var basequeryurl= '/exist/rest/db/ukpga';
 	var modules= 'import module namespace response = "http://exist-db.org/xquery/response";';
     var namespaceDec='declare namespace akn="http://docs.oasis-open.org/legaldocml/ns/akn/3.0/CSD11";declare namespace response="http://exist-db.org/xquery/response";';
-	var flwrStart='for $section in ';
+	var flwrStart='for $entry in ';
     var collection = 'collection(\'ukpga\')';
 	var headers = ' let $headers :=(response:set-header("Access-Control-Allow-Origin", "*"))';
-    var returnPhrase = ' return <entry>{$section}</entry>';
+    var returnPhrase = ' return <div class="entry">{$entry}</div>';
     //Return public API
         return({
             queryUKLeg: queryUKLeg,
-            getQueryURL: getQueryURL,
             getCombinedQuery : function(inputXPath){
                 return "collection('ukpga')"+inputXPath;
             }
             });
-        /*
-         * PUBLIC METHODS
-         */
 
-        function getQueryURL(inputXPath){
-            return basedomain+basequeryurl+'?_query='+window.encodeURIComponent(namespaceDec+flwrStart+collection+inputXPath+headers+returnPhrase);
-        }
-        function queryUKLeg(inputXPath){
+        function queryUKLeg(inputXPath, options){
             var request = $http({
                         method: "get",
                         url: basedomain + basequeryurl,
                         params: {
-                            _query: namespaceDec+flwrStart+collection+inputXPath+headers+returnPhrase
+                            _query: namespaceDec+flwrStart+collection+inputXPath+headers+returnPhrase,
+                            _howmany: options.howManyResults
                         }
                     });
 
