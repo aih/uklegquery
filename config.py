5276775acf47f001442cc3572e836127c17edbeb
aih <aih@tabulaw.com>
2014-11-26 16:35:21 -0800

    Add About modal

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-    VERSION = '0.0.5'
+    VERSION = '0.0.6'
 
 class ProductionConfig(Config):
     DEBUG = False
